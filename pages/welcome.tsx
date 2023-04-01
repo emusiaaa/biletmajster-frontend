f766a6522542354ae6aade128a5a87f8d63bfbc1
emusiaaa <emmo.lev@gmail.com>
2023-04-01 04:42:05 +0200

    welcome page

--- pages/welcome.tsx
+++ pages/welcome.tsx
@@ -17,63 +17,73 @@ export default function Home() {
             </Head>
 
             <main>
-                <Grid container alignItems="center"
-                      justifyContent="center">
-                    <Grid item xs={12}>
 
+                <Grid container alignItems="center"
+                      sx={{
+                          mt: 8,
+                          width: {xs:'80%', md:'60%'}, minHeight:'400px',
+                          border: '4px solid #A2ADCD',
+                          borderRadius: '30px',
+                      }}
+                      justifyContent="center" direction="column">
+                    <Grid item xs={12}>
+                        <Grid container alignItems="center" >
+                            <Grid item>
+                                <BookOnlineRoundedIcon sx={{
+                                    display: { xs: 'flex', md: 'flex' },
+                                    fontSize:{xs:'50px', md:'80px'},
+                                    color: 'white',
+                                    mr:'10px'}} />
+                            </Grid>
+                            <Grid item>
+                                <Typography
+                                    noWrap
+                                    sx={{
+                                        display: { xs: 'flex', md: 'flex' },
+                                        fontFamily: 'monospace',
+                                        fontSize: {xs:'50px', md:'80px'},
+                                        fontWeight: 700,
+                                        letterSpacing: '.3rem',
+                                        color: 'white',
+                                        textDecoration: 'none',
+                                    }}
+                                >
+                                    DIONIZOS
+                                </Typography>
+                            </Grid>
+                        </Grid>
                     </Grid>
-                    <BookOnlineRoundedIcon sx={{
-                        display: { xs: 'flex', md: 'flex' },
-                        fontSize:{xs:'50px', md:'80px'},
-                        color: 'white',
-                        mr:'10px'}} />
-                    <Typography
-                        noWrap
-                        component="a"
-                        href="/"
-                        sx={{
-                            mr: 2,
-                            display: { xs: 'flex', md: 'flex' },
-                            fontFamily: 'monospace',
-                            fontSize: {xs:'50px', md:'80px'},
-                            fontWeight: 700,
-                            letterSpacing: '.3rem',
-                            color: 'white',
-                            textDecoration: 'none',
-                        }}
-                    >
-                        DIONIZOS
-                    </Typography>
-                    <Typography
-                        noWrap
-                        component="a"
-                        href="/"
-                        sx={{
-                            mr: 2,
-                            display: { xs: 'flex', md: 'flex' },
-                            fontFamily: 'monospace',
-                            fontSize: {xs:'20px', md:'40px'},
-                            fontWeight: 700,
-                            letterSpacing: '.3rem',
-                            color: 'white',
-                            textDecoration: 'none',
-                        }}
-                    >
-                        FOR EVENT CREATORS
-                    </Typography>
-                    <Grid container>
-                        <Grid item sx={12}>
+                    <Grid item xs={12}>
+                        <Typography
+                            noWrap
+                            sx={{
+                                display: { xs: 'flex', md: 'flex' },
+                                fontFamily: 'monospace',
+                                fontSize: {xs:'20px', md:'30px'},
+                                fontWeight: 700,
+                                letterSpacing: '.3rem',
+                                color: 'white',
+                                textDecoration: 'none',
+                            }}
+                        >
+                            FOR EVENT CREATORS
+                        </Typography>
+                    </Grid>
+
+
+                    <Grid container alignItems="center" direction="column">
+                        <Grid item xs={12}>
                             <Button
                                 variant="contained"
-                                sx={{ my: 2, color: 'white', display: 'block',
-                                    bgcolor:'#73A896', mr:'10px', ":hover":{bgcolor:'#A2ADCD'}}}
+                                sx={{ color: 'white', display: 'block',
+                                    bgcolor:'#73A896', mt:'10px', ":hover":{bgcolor:'#A2ADCD'}}}
                             >Log in</Button>
                         </Grid>
-                        <Grid item>
+                        <Grid item xs={12}>
                             <Button
                                 variant="contained"
-                                sx={{ my: 2, color: 'white', display: 'block',
-                                    bgcolor:'#73A896', mr:'10px', ":hover":{bgcolor:'#A2ADCD'}}}
+                                sx={{color: 'white', display: 'block',
+                                    bgcolor:'#73A896', mt:'10px', mb:'20px',":hover":{bgcolor:'#A2ADCD'}}}
                             >Register</Button>
                         </Grid>
                     </Grid>
