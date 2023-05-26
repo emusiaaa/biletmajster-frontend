778abd51c901ccd61ee7d892a33ce34f28cceeea
JB <7b187c04-cf8d-4293-af69-6f7214332dd2@simplelogin.com>
2023-05-26 15:08:50 +0200

    Added new PhotoManager to Edit Event form

--- pages/events/edit.tsx
+++ pages/events/edit.tsx
@@ -35,7 +35,7 @@ import { useApiClient } from "../../functions/useApiClient";
 import { useRouter } from "next/router";
 import { Map } from "@/components/Map";
 import { firstLoadState } from "recoil/firstLoadState";
-import { PhotoSelector } from "@/components/PhotoSelector";
+import { PhotoManager } from "@/components/PhotoManager";
 
 const ITEM_HEIGHT = 48;
 const ITEM_PADDING_TOP = 8;
@@ -48,14 +48,6 @@ const MenuProps = {
   },
 };
 
-function getStyles(name: string, personName: readonly string[], theme: Theme) {
-  return {
-    fontWeight:
-      personName.indexOf(name) === -1
-        ? theme.typography.fontWeightRegular
-        : theme.typography.fontWeightMedium,
-  };
-}
 export default function Categories() {
   const [loaded, _] = useRecoilState(firstLoadState);
   const [sessionToken, setSessionToken] = useRecoilState(sessionTokenState);
@@ -72,6 +64,16 @@ export default function Categories() {
   const [selectedCategories, setSelectedCategories] = useState<Category[]>([]);
   const [errors, setErrors] = useState<ValidationErrors<EventForm>>({});
   const [placeSchema, setPlaceSchema] = useState<string | undefined>(undefined);
+
+  const applyPlaceSchema = (newSchema: File | undefined) => {
+    if (newSchema === undefined)
+      setPlaceSchema(undefined);
+    else {
+      const reader = new FileReader();
+      reader.readAsDataURL(newSchema);
+      reader.onload = () => setPlaceSchema(reader.result as string | undefined);
+    }
+  }
 
   const apiClient = useApiClient();
   const theme = useTheme();
@@ -397,9 +399,13 @@ export default function Categories() {
                   </Grid>
                 </Grid>
                 <div style={{ marginBottom: 16 }}>
-                  <PhotoSelector
-                    image={placeSchema}
-                    setImage={setPlaceSchema}
+                  <PhotoManager
+                    title="Place schema"
+                    maxImages={1}
+                    imageSrcs={placeSchema === undefined ? [] : [placeSchema]}
+                    addImage={file => applyPlaceSchema(file)}
+                    removeByIndex={() => applyPlaceSchema(undefined)}
+                    allowedTypes={["png"]}
                   />
                 </div>
                 <Button
