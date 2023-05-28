9a5b6558ed14b30c3d34421783335c4118625a33
JB <7b187c04-cf8d-4293-af69-6f7214332dd2@simplelogin.com>
2023-05-29 01:03:52 +0200

    end.

--- e2e/login_add_edit.ts
+++ e2e/login_add_edit.ts
@@ -1,5 +1,6 @@
 import { By, Key, Builder, WebDriver, until } from 'selenium-webdriver';
 import { createDriver } from './utils/createDriver';
+import { forMs } from './utils/forMs';
 
 // CHROMEDRIVER_EXE - path to chrome driver
 // CHROME_EXE - path to chrome binary
@@ -18,69 +19,91 @@ async function test_case() {
 	const timeout = setTimeout(function () {
 		driver.quit();
 		throw ("Timeout");
-	}, 20000);
+	}, 120000);
+
+	// start the backend
+	await fetch("https://biletmajster.azurewebsites.net/events");
 
 	// main test
 	await driver.get("http://localhost:3000/login");
 
 	// wait for login form, then log in
-	await driver.wait(until.elementIsVisible(await driver.findElement(By.linkText("Log in"))));
+	await driver.wait(until.elementIsVisible(await driver.findElement(By.xpath("//button[contains(., 'Log in')]"))));
 	await driver.findElement(By.id("email")).sendKeys(user);
 	await driver.findElement(By.id("password")).sendKeys(pass, Key.ENTER);
 
 	// Go to Add Event
-	await driver.wait(until.elementIsVisible(await driver.findElement(By.xpath("//button[contains(., 'Add new event)]"))));
+	await driver.wait(until.urlIs("http://localhost:3000/dashboard"));
+	await driver.wait(until.elementIsVisible(await driver.findElement(By.xpath("//button[contains(., 'Add new event')]"))), 10000);
 	await driver.findElement(By.xpath("//button[contains(., 'Add new event')]")).click();
 
 	// Create a sample event - will redirect to My Events
+	await driver.wait(until.urlIs("http://localhost:3000/events/add"));
 	const eventName = "seleniumEvent" + String(Math.floor(Math.random() * 1000000));
+	await forMs(1000);
 	// title
-	await driver.findElement(By.xpath("//*[@id=':r2:']")).sendKeys(eventName);
+	await driver.wait(until.elementIsVisible(await driver.findElement(By.xpath("//div[contains(., 'Nazwa eventu')]/input"))), 10000);
+	await driver.findElement(By.xpath("//div[contains(., 'Nazwa eventu')]/input")).sendKeys(eventName);
 	// max places
-	await driver.findElement(By.xpath("//*[@id=':r8:']")).sendKeys("100");
+	await driver.findElement(By.xpath("//div[contains(., 'Max places')]/input")).sendKeys("100");
 	// latitude and longitude
-	await driver.findElement(By.xpath("//*[@id=':r9:']")).sendKeys("52", Key.TAB, "21");
+	await driver.findElement(By.xpath("//div[contains(., 'Lat')]/input")).sendKeys("52", Key.TAB, "21");
 	// description
-	await driver.findElement(By.xpath("//*[@id=':r3:']")).sendKeys("Selenium");
+	await driver.findElement(By.xpath("//div[contains(., 'Short description of event')]/textarea")).sendKeys("Selenium");
 	// start and end date
 	await driver.actions().sendKeys(Key.TAB).perform();
-	await driver.actions().sendKeys(Key.ENTER).perform();
-	await driver.actions().sendKeys(Key.ENTER).perform();
+	await forMs(70);
 	await driver.actions().sendKeys(Key.TAB).perform();
-	await driver.actions().sendKeys(Key.TAB).perform();
-	await driver.actions().sendKeys(Key.TAB).perform();
+	await forMs(70);
 	await driver.actions().sendKeys(Key.ENTER).perform();
+	await forMs(70);
+	await driver.actions().sendKeys(Key.ENTER).perform();
+	await forMs(70);
+	await driver.actions().sendKeys(Key.TAB).perform();
+	await forMs(70);
+	await driver.actions().sendKeys(Key.TAB).perform();
+	await forMs(70);
+	await driver.actions().sendKeys(Key.TAB).perform();
+	await forMs(70);
+	await driver.actions().sendKeys(Key.ENTER).perform();
+	await forMs(70);
 	await driver.actions().sendKeys(Key.ARROW_RIGHT).perform();
+	await forMs(70);
 	await driver.actions().sendKeys(Key.ENTER).perform();
+	await forMs(70);
 	// dropdown (add category)
 	await driver.findElement(By.xpath("//*[@data-testid='select']")).click();
 	await driver.actions().sendKeys(Key.ARROW_DOWN).perform();
 	await driver.actions().sendKeys(Key.ENTER).perform();
 	await driver.actions().sendKeys(Key.ESCAPE).perform();
-	await driver.findElement(By.xpath("//*[@data-testid='open-form-btn']")).click();
 	// submit new event
-	await driver.findElement(By.xpath("//button[@type='submit]")).click();
+	await driver.findElement(By.xpath("//button[@type='submit']")).click();
 
 
 	// Wait until event card is present, then click Edit on the event.
-	// await driver.wait(until.urlIs("http://localhost:3000/events/my"));
-	await driver.wait(until.elementIsVisible(await driver.findElement(By.xpath('//div[contains(@class, "MuiCard-root") and contains(., "' + eventName + '")]'))));
-	await driver.findElement(By.xpath('//div[contains(@class, "MuiCard-root") and contains(., "' + eventName + '")]//button[@data-testid="edit-event-button"]')).click();
+	await driver.wait(until.urlIs("http://localhost:3000/events/my"));
+	await forMs(1000);
+	await driver.wait(until.elementIsVisible(await driver.findElement(By.xpath('//div[contains(., "' + eventName + '")]'))), 5000);
+	await forMs(70);
+	await driver.findElement(By.xpath('//div[contains(@class, "MuiCard") and contains(., "' + eventName + '")]//button[@data-testid="edit-event-button"]')).click();
 
 	// Change title of the event - will redirect to dashboard
+	await driver.wait(until.urlContains("http://localhost:3000/events/edit"));
 	const newEventName = eventName + "edited";
-	await driver.wait(until.elementTextIs(await driver.findElement(By.xpath("//*[@id=':r2:']")), eventName)); // wait until it loads
-	await driver.findElement(By.xpath("//*[@id=':r2:']")).click();
-	await driver.findElement(By.xpath("//*[@id=':r2:']")).clear();
-	await driver.findElement(By.xpath("//*[@id=':r2:']")).sendKeys(newEventName);
-	await driver.findElement(By.xpath("//button[@type='submit]")).click();
+	// await driver.wait(until.elementTextIs(await driver.findElement(By.xpath("//div[contains(., 'Nazwa eventu')]/input")), eventName)); // wait until it loads
+	await forMs(3000);
+	await driver.findElement(By.xpath("//div[contains(., 'Nazwa eventu')]/input")).click();
+	await driver.findElement(By.xpath("//div[contains(., 'Nazwa eventu')]/input")).sendKeys("edited");
+	await driver.findElement(By.xpath("//button[@type='submit']")).click();
 
 	// Go to My Events
 	await driver.wait(until.urlIs('http://localhost:3000/dashboard'));
 	await driver.findElement(By.xpath("//button[contains(., 'My events')]")).click();
 
 	// Check if the card with the new name is present
-	await driver.wait(until.elementIsVisible(await driver.findElement(By.xpath('//div[contains(@class, "MuiCard-root") and contains(., "' + newEventName + '")]'))));
+	await driver.wait(until.urlIs("http://localhost:3000/events/my"));
+	await forMs(2000);
+	await driver.wait(until.elementIsVisible(await driver.findElement(By.xpath('//*[contains(., "' + newEventName + '")]'))), 10000);
 
 	driver.close();
 	driver.quit();
