b59f2627ccf48f1a9dc87ea3c720991d94e11c6d
Mateusz Chudek <m.chudek@kaft.pl>
2023-05-29 08:32:39 +0200

    Cleaned removeOldEvent e2e test

--- e2e/todo/removeOldEvents.js
+++ e2e/todo/removeOldEvents.js
@@ -13,26 +13,23 @@ async function test_case() {
   await driver.findElement(By.linkText("LOG IN")).click();
   await driver.findElement(By.id("email")).sendKeys("tommy.sh500@gmail.com", Key.TAB);
   await driver.findElement(By.id("password")).sendKeys("password1234", Key.TAB, Key.TAB, Key.TAB, Key.ENTER, Key.ARROW_DOWN, Key.ARROW_DOWN, Key.ENTER, Key.TAB, Key.TAB, Key.TAB);
-  // await driver.findElement(By.partialLinkText("Log")).click();
   await driver.findElement(By.xpath("//button[normalize-space()='Log in']")).click();
-  await forMs(1000);
-
-  // navigate to my events
-  await driver.findElement(By.xpath("//button[normalize-space()='My events']")).click();
   await forMs(2000);
 
-  // delete created event
-  // await driver.findElement(By.xpath("//*[@aria-label='delete-event']")).click();
-  // await driver.findElement(By.xpath("//*[@data-testid='delete-event-button' and ancestor::*[contains(text(),'SeleniumTestEvent2')]]")).click();
-  // await driver.findElement(By.xpath('//div[contains(@class, "MuiCard-root") and contains(., "BSeleniumTestEvent1212312334")]//button[@data-testid="delete-event-button"]')).click();
-  // await driver.findElement(By.xpath('//div[contains(@class, "MuiCard-root") and contains(., "EVENT TITLE")]//button[@data-testid="edit-event-button"]')).click();
-  await driver.wait(until.elementIsVisible(await driver.findElement(
-    By.xpath('//div[contains(@class, "MuiCard-root") and contains(., "BSeleniumTestEvent1212312334")]//button[@data-testid="delete-event-button"]')))).click();
+  // navigate to my events
+  await driver.wait(until.elementIsVisible(await driver.findElement(By.xpath("//button[normalize-space()='My events']")))).click();
+  await forMs(2000);
 
+  // delete event
+  await driver.wait(until.elementIsVisible(await driver.findElement(
+    By.xpath('//div[contains(@class, "MuiCard-root") and contains(., "Spotkanie w break5")]//button[@data-testid="delete-event-button"]')))).click();
+  await forMs(4000);
 
   // refresh my events page
-  // await driver.findElement(By.xpath("//button[normalize-space()='Add new event']")).click();
-  // await driver.findElement(By.xpath("//button[normalize-space()='My events']")).click();
+  await driver.findElement(By.xpath("//button[normalize-space()='Dashboard']")).click();
+  await forMs(4000);
+  await driver.findElement(By.xpath("//button[normalize-space()='My events']")).click();
+  await forMs(2000);
 
   await forMs(4000);
   await driver.quit();
