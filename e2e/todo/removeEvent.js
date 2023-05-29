ea562b0663159db216609ecd4a2d23c892d7ca4b
Mateusz Chudek <m.chudek@kaft.pl>
2023-05-29 08:42:13 +0200

    Refactor removeEvent e2e test

--- e2e/todo/removeEvent.js
+++ e2e/todo/removeEvent.js
@@ -1,5 +1,5 @@
 
-const { By, Key, Builder } = require("selenium-webdriver");
+const { By, Key, Builder, until } = require("selenium-webdriver");
 require("chromedriver");
 
 const forMs = async (ms) => new Promise(resolve => setTimeout(() => resolve(), ms));
@@ -15,16 +15,16 @@ async function test_case() {
   await driver.findElement(By.id("password")).sendKeys("password1234", Key.TAB, Key.TAB, Key.TAB, Key.ENTER, Key.ARROW_DOWN, Key.ARROW_DOWN, Key.ENTER, Key.TAB, Key.TAB, Key.TAB);
   // await driver.findElement(By.partialLinkText("Log")).click();
   await driver.findElement(By.xpath("//button[normalize-space()='Log in']")).click();
-  await forMs(1000);
+  await forMs(2000);
 
   // navigate to my events
   await driver.findElement(By.xpath("//button[normalize-space()='Add new event']")).click();
-  await forMs(1000);
+  await forMs(2000);
 
   // add new event
 
   // title
-  await driver.findElement(By.xpath("//*[@id=':r2:']")).sendKeys("BSeleniumTestEvent1212312334");
+  await driver.findElement(By.xpath("//*[@id=':r2:']")).sendKeys("SeleniumCreateDeleteEvent2");
   // max places
   await driver.findElement(By.xpath("//*[@id=':r8:']")).sendKeys("100");
   // latitude and longitude
@@ -51,20 +51,19 @@ async function test_case() {
 
   // submit new event
   await driver.findElement(By.xpath("//*[@data-testid='add-btn']")).click();
-  await forMs(1000);
+  await forMs(4000);
 
-  // click on OK button with enter - chyba niepotrzebne
-  // await driver.actions().sendKeys(Key.ENTER).perform();
 
-  // navigate to my events
-  await driver.findElement(By.xpath("//button[normalize-space()='My events']")).click();
-
-  // delete created event
-  await driver.findElement(By.xpath("//*[@data-testid='delete-event-button' and ancestor[normalize-space()='BSeleniumTestEvent1212312334']")).click();
+  // delete event
+  await driver.wait(until.elementIsVisible(await driver.findElement(
+    By.xpath('//div[contains(@class, "MuiCard-root") and contains(., "SeleniumCreateDeleteEvent2")]//button[@data-testid="delete-event-button"]')))).click();
+  await forMs(4000);
 
   // refresh my events page
-  // await driver.findElement(By.xpath("//button[normalize-space()='Add new event']")).click();
-  // await driver.findElement(By.xpath("//button[normalize-space()='My events']")).click();
+  await driver.findElement(By.xpath("//button[normalize-space()='Dashboard']")).click();
+  await forMs(4000);
+  await driver.findElement(By.xpath("//button[normalize-space()='My events']")).click();
+  await forMs(2000);
 
   await forMs(4000);
   await driver.quit();
